b2b56d6c5cd665d70fa3e45d8d3a83c11d3b7eb3
hoale0507 <hoale090293@gmail.com>
2022-01-14 21:23:24 +0700

    pass all cases

--- src/NextDayCalculator.java
+++ src/NextDayCalculator.java
@@ -3,14 +3,18 @@ public class NextDayCalculator {
     public static final String CONCATENATION = "/";
 
     public static String calculateNextDay(int day, int month, int year){
-
-//        if(day == 31){
-//            return 1 + "/" + (month+1) + "/"+year;
-//        }
-//        if(day == 30 && month == 4){
-//            return 1 + "/" + (month+1) + "/" + year;
-//        }
-        
+        if(day == 31 && month != 12){
+            return 1 + NextDayCalculator.CONCATENATION + (month+1) + NextDayCalculator.CONCATENATION +year;
+        }
+        if(day == 30 && (month == 4 || month == 6 || month == 9 || month == 11)){
+            return 1 + NextDayCalculator.CONCATENATION + (month+1) + NextDayCalculator.CONCATENATION + year;
+        }
+        if(day == 28 || day == 29 && month == 2){
+            return 1 + NextDayCalculator.CONCATENATION + 3 + NextDayCalculator.CONCATENATION + year;
+        }
+        if(day == 31 && month == 12){
+            return 1 + CONCATENATION + 1 + CONCATENATION + (year +1);
+        }
         return (day+1)+ CONCATENATION +month+ CONCATENATION +year;
     }
 }
--- test/NextDayCalculatorTest.java
+++ test/NextDayCalculatorTest.java
@@ -15,26 +15,59 @@ class NextDayCalculatorTest {
         assertEquals(expected, result);
     }
 
-//    @Test
-//    @DisplayName("case day 31/1/2021")
-//    void testDay31Month1Year2021(){
-//        int day = 31;
-//        int month = 1;
-//        int year = 2021;
-//        String expected = "1/2/2021";
-//        String result = NextDayCalculator.calculateNextDay(day, month, year);
-//        assertEquals(expected, result);
-//    }
-//
-//    @Test
-//    @DisplayName("case day 30/4/2021")
-//    void testDay30Month4Year2021(){
-//        int day = 30;
-//        int month = 4;
-//        int year = 2021;
-//        String expected = "1/5/2021";
-//        String result = NextDayCalculator.calculateNextDay(day, month, year);
-//        assertEquals(expected, result);
-//    }
+    @Test
+    @DisplayName("case day 31/1/2021")
+    void testDay31Month1Year2021(){
+        int day = 31;
+        int month = 1;
+        int year = 2021;
+        String expected = "1/2/2021";
+        String result = NextDayCalculator.calculateNextDay(day, month, year);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("case day 30/4/2021")
+    void testDay30Month4Year2021(){
+        int day = 30;
+        int month = 4;
+        int year = 2021;
+        String expected = "1/5/2021";
+        String result = NextDayCalculator.calculateNextDay(day, month, year);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("case day 28/02/2021")
+    void testDay28Month12Year2021(){
+        int day = 28;
+        int month = 2;
+        int year = 2021;
+        String expected = "1/3/2021";
+        String result = NextDayCalculator.calculateNextDay(day, month, year);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("case day 31/12/2021")
+    void testDay31Month12Year2021(){
+        int day = 31;
+        int month = 12;
+        int year = 2021;
+        String expected = "1/1/2022";
+        String result = NextDayCalculator.calculateNextDay(day, month, year);
+        assertEquals(expected, result);
+    }
+    @Test
+    @DisplayName("case day 29/02/2024")
+    void testDay29Month2Year2024(){
+        int day = 29;
+        int month = 2;
+        int year = 2024;
+        String expected = "1/3/2024";
+        String result = NextDayCalculator.calculateNextDay(day, month, year);
+        assertEquals(expected, result);
+    }
+
 
 }
